cf90b1342816e1270cbc0bdd2007c0db2e33aa6f
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2023-01-06 10:23:22 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygeocoder
+
+# Load the image
+image = Image.open("image.jpg")
+
+# Extract the EXIF data from the image
+exif_data = image._getexif()
+
+# Get the GPS information from the EXIF data
+gps_info = exif_data[34853]
+
+# Extract the latitude and longitude from the GPS information
+latitude = gps_info[2][0][0] / gps_info[2][0][1]
+longitude = gps_info[4][0][0] / gps_info[4][0][1]
+
+# Use PyGeoCoder to reverse geocode the coordinates and get the location
+location = pygeocoder.Geocoder.reverse_geocode(latitude, longitude)
+
+# Print the location
+print(location)
